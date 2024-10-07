c78a3e74c3e228e02575293ca1052407a031bedc
up1 <somkiat.p@gmail.com>
2024-10-07 22:14:05 +0700

    Demo env with spring boot

--- src/main/java/com/example/docker01/Hello.java
+++ src/main/java/com/example/docker01/Hello.java
@@ -1,14 +1,18 @@
 package com.example.docker01;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
 
+    @Value("${auth.server}")
+    private String url;
+
     @GetMapping("/hello")
     public HelloResponse sayHi() {
-        return new HelloResponse("Helloooo Spring Boot");
+        return new HelloResponse("Hello Spring Boot", url);
     }
 
 }
--- src/main/java/com/example/docker01/HelloResponse.java
+++ src/main/java/com/example/docker01/HelloResponse.java
@@ -2,12 +2,14 @@ package com.example.docker01;
 
 public class HelloResponse {
     private String message;
+    private String url;
 
     public HelloResponse() {
     }
 
-    public HelloResponse(String message) {
+    public HelloResponse(String message, String url) {
         this.message = message;
+        this.url = url;
     }
 
     public String getMessage() {
@@ -16,5 +18,13 @@ public class HelloResponse {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 }
