20fc7f5161f24cad6999f74a7986cb72fed54a1a
up1 <somkiat.p@gmail.com>
2024-10-07 11:58:25 +0700

    java

---
+++ src/main/java/com/example/docker01/Hello.java
@@ -0,0 +1,14 @@
+package com.example.docker01;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public HelloResponse sayHi() {
+        return new HelloResponse("Hell Spring Boot");
+    }
+
+}
